6eac4fe1ba40289c6c1b31d44ca6fa959790e82c
JandroMejia97 <alejhrndz97@outlook.com>
2019-10-20 01:13:24 -0300

    Añadiendo NotFoundComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { HomeComponent } from './content/home/home.component';
+import { AboutUsComponent } from './content/about-us/about-us.component';
+import { GameComponent } from './content/game/game.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./content/content.module').then(c => c.ContentModule)
+    component: HomeComponent
+  },
+  {
+    path: 'game',
+    component: GameComponent
+  },
+  {
+    path: 'about',
+    component: AboutUsComponent
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
   }
 ];
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,18 +5,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [ ToolbarComponent, FooterComponent],
+  declarations: [ ToolbarComponent, FooterComponent, NotFoundComponent],
   imports: [
     CommonModule,
     NgbModule
   ],
   exports: [
     ToolbarComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent
   ]
 })
 export class SharedModule { }
