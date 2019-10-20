2bae53a33312fa13252c847e47becbcaf31a74e4
JandroMejia97 <alejhrndz97@outlook.com>
2019-10-20 03:31:55 -0300

    Update mock-personas.ts

--- src/app/data/mock-personas.ts
+++ src/app/data/mock-personas.ts
@@ -2,75 +2,75 @@ import { Persona } from '../models/Persona';
 
 export const PERSONAS: Persona[] = [
     {
-      nombre: "Marengo, Augusto",
+      nombre: 'Marengo, Augusto',
       edad: 25,
-      lugarNacimiento: "Trenque Lauquen",
-      carrera: "Ing. Mecánica",
-      razon: "",
-      foto: ""
+      lugarNacimiento: 'Trenque Lauquen',
+      carrera: 'Ing. Mecánica',
+      razon: '',
+      foto: ''
     },
     {
-      nombre: "Cunha de Meira, Katylin Rainara",
+      nombre: 'Cunha de Meira, Katylin Rainara',
       edad: 22,
-      lugarNacimiento: "São Roque, São Paulo, Brasil",
-      carrera: "Ing. Mecánica",
-      razon: "Me encanta el trabajo en equipo y el aprendizaje.",
-      foto: "https://drive.google.com/uc?export=view&id=1r9TqXkWo2PPoCpjfUzmgUzZh4IGhYtxX"
+      lugarNacimiento: 'São Roque, São Paulo, Brasil',
+      carrera: 'Ing. Mecánica',
+      razon: 'Me encanta el trabajo en equipo y el aprendizaje.',
+      foto: 'https://drive.google.com/uc?export=view&id=1r9TqXkWo2PPoCpjfUzmgUzZh4IGhYtxX'
     },
     {
-      nombre: "Custodio, Walter",
+      nombre: 'Custodio, Walter',
       edad: 25,
-      lugarNacimiento: "Trenque Lauquen",
-      carrera: "Ing. Mecánica",
-      razon: "",
-      foto: ""
+      lugarNacimiento: 'Trenque Lauquen',
+      carrera: 'Ing. Mecánica',
+      razon: '',
+      foto: ''
     },
     {
-      nombre: "Daruich, Valentina",
+      nombre: 'Daruich, Valentina',
       edad: 25,
-      lugarNacimiento: "Trenque Lauquen",
-      carrera: "Ing. Mecánica",
-      razon: "",
-      foto: ""
+      lugarNacimiento: 'Trenque Lauquen',
+      carrera: 'Ing. Mecánica',
+      razon: '',
+      foto: ''
     },
     {
-      nombre: "Difeo, Martin",
+      nombre: 'Difeo, Martin',
       edad: 25,
-      lugarNacimiento: "Trenque Lauquen",
-      carrera: "Ing. Mecánica",
-      razon: "",
-      foto: ""
+      lugarNacimiento: 'Trenque Lauquen',
+      carrera: 'Ing. Mecánica',
+      razon: '',
+      foto: ''
     },
     {
-      nombre: "Hernández Mejía, Carlos Alejandro",
+      nombre: 'Hernández Mejía, Carlos Alejandro',
       edad: 25,
-      lugarNacimiento: "Trenque Lauquen",
-      carrera: "Ing. Mecánica",
-      razon: "",
-      foto: ""
+      lugarNacimiento: 'Trenque Lauquen',
+      carrera: 'Ing. Mecánica',
+      razon: '',
+      foto: ''
     },
     {
-      nombre: "Lacava, Tomás",
+      nombre: 'Lacava, Tomás',
       edad: 25,
-      lugarNacimiento: "Trenque Lauquen",
-      carrera: "Ing. Mecánica",
-      razon: "",
-      foto: ""
+      lugarNacimiento: 'Trenque Lauquen',
+      carrera: 'Ing. Mecánica',
+      razon: '',
+      foto: ''
     },
     {
-      nombre: "Ortíz, Gabriel",
+      nombre: 'Ortíz, Gabriel',
       edad: 25,
-      lugarNacimiento: "Trenque Lauquen",
-      carrera: "Ing. Mecánica",
-      razon: "",
-      foto: ""
+      lugarNacimiento: 'Trenque Lauquen',
+      carrera: 'Ing. Mecánica',
+      razon: '',
+      foto: ''
     },
     {
-      nombre: "Salto, Emanuel",
+      nombre: 'Salto, Emanuel',
       edad: 25,
-      lugarNacimiento: "Trenque Lauquen",
-      carrera: "Ing. Mecánica",
-      razon: "",
-      foto: ""
+      lugarNacimiento: 'Trenque Lauquen',
+      carrera: 'Ing. Mecánica',
+      razon: '',
+      foto: ''
     }
   ];
